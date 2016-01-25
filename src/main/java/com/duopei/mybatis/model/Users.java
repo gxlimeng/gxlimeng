182b6757ab02c50365b3e8423b5f3074feba75b0
pintend <limeng530_1987@163.com>
2016-01-25 17:36:32 +0800

    mybatis log4j
    mybatis log4j 配置

---
+++ src/main/java/com/duopei/mybatis/model/Users.java
@@ -0,0 +1,25 @@
+package com.duopei.mybatis.model;
+
+public class Users {
+
+	private int id;
+
+	private String userName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
